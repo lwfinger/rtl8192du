d4a43ea48ed899bc0b6fda382690617047597206
Larry Finger <Larry.Finger@lwfinger.net>
2014-03-15 22:10:18 -0500

    Remove undefined CONFIG_P2P_IPS
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>

--- core/rtw_pwrctrl.c
+++ core/rtw_pwrctrl.c
@@ -122,9 +122,7 @@ static bool rtw_pwr_unassociated_idle(struct rtw_adapter *adapter)
 	if (check_fwstate(pmlmepriv, WIFI_ASOC_STATE | WIFI_SITE_MONITOR) ||
 	    check_fwstate(pmlmepriv, WIFI_UNDER_LINKING | WIFI_UNDER_WPS) ||
 	    check_fwstate(pmlmepriv, WIFI_AP_STATE) ||
-#if defined(CONFIG_P2P) && defined(CONFIG_IOCTL_CFG80211) && defined(CONFIG_P2P_IPS)
-	    pcfg80211_wdinfo->is_ro_ch ||
-#elif defined(CONFIG_P2P)
+#if defined(CONFIG_P2P)
 	    !rtw_p2p_chk_state(pwdinfo, P2P_STATE_NONE) ||
 #endif
 	    check_fwstate(pmlmepriv, WIFI_ADHOC_MASTER_STATE | WIFI_ADHOC_STATE))
@@ -144,9 +142,7 @@ static bool rtw_pwr_unassociated_idle(struct rtw_adapter *adapter)
 		if (check_fwstate(b_pmlmepriv, WIFI_ASOC_STATE | WIFI_SITE_MONITOR) ||
 		    check_fwstate(b_pmlmepriv, WIFI_UNDER_LINKING | WIFI_UNDER_WPS) ||
 		    check_fwstate(b_pmlmepriv, WIFI_AP_STATE) ||
-#if defined(CONFIG_P2P) && defined(CONFIG_IOCTL_CFG80211) && defined(CONFIG_P2P_IPS)
-		    b_pcfg80211_wdinfo->is_ro_ch ||
-#elif defined(CONFIG_P2P)
+#if defined(CONFIG_P2P)
 		    !rtw_p2p_chk_state(b_pwdinfo, P2P_STATE_NONE) ||
 #endif
 		    check_fwstate(b_pmlmepriv, WIFI_ADHOC_MASTER_STATE | WIFI_ADHOC_STATE)) {
@@ -351,9 +347,7 @@ void rtw_lps_enter(struct rtw_adapter *padapter)
 		     check_fwstate(b_pmlmepriv, WIFI_UNDER_LINKING | WIFI_UNDER_WPS) ||
 		     check_fwstate(b_pmlmepriv, WIFI_AP_STATE) ||
 		     check_fwstate(b_pmlmepriv, WIFI_ADHOC_MASTER_STATE | WIFI_ADHOC_STATE) ||
-#if defined(CONFIG_P2P) && defined(CONFIG_IOCTL_CFG80211) && defined(CONFIG_P2P_IPS)
-		    b_pcfg80211_wdinfo->is_ro_ch ||
-#elif defined(CONFIG_P2P)
+#if defined(CONFIG_P2P)
 		    !rtw_p2p_chk_state(b_pwdinfo, P2P_STATE_NONE) ||
 #endif
 		    rtw_is_scan_deny(buddy))

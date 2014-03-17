337966561a15f33312935adc54e3daa445da7c12
Stefan Lippers-Hollmann <s.l-h@gmx.de>
2014-03-17 06:30:01 +0100

    rtl8192du: drop unused function process_p2p_devdisc_resp()
    Signed-off-by: Stefan Lippers-Hollmann <s.l-h@gmx.de>

--- include/rtw_p2p.h
+++ include/rtw_p2p.h
@@ -41,7 +41,6 @@ u32 build_provdisc_resp_wfd_ie(struct wifidirect_info *pwdinfo, u8 *pbuf);
 u32 process_probe_req_p2p_ie(struct wifidirect_info *pwdinfo, u8 *pframe, uint len);
 u32 process_assoc_req_p2p_ie(struct wifidirect_info *pwdinfo, u8 *pframe, uint len, struct sta_info *psta);
 u32 process_p2p_devdisc_req(struct wifidirect_info *pwdinfo, u8 *pframe, uint len);
-u32 process_p2p_devdisc_resp(struct wifidirect_info *pwdinfo, u8 *pframe, uint len);
 u8 process_p2p_provdisc_req(struct wifidirect_info *pwdinfo,  u8 *pframe, uint len);
 u8 process_p2p_provdisc_resp(struct wifidirect_info *pwdinfo,  u8 *pframe);
 u8 process_p2p_group_negotation_req(struct wifidirect_info *pwdinfo, u8 *pframe, uint len);

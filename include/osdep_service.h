36c587e05c8e60e361f5f5e536326dcfd617e9c0
Stefan Lippers-Hollmann <s.l-h@gmx.de>
2014-03-22 03:03:42 +0100

    rtl8192du: simplify _RND128()
    Signed-off-by: Stefan Lippers-Hollmann <s.l-h@gmx.de>

--- include/osdep_service.h
+++ include/osdep_service.h
@@ -173,13 +173,7 @@ static inline u32 _RND8(u32 sz)
 
 static inline u32 _RND128(u32 sz)
 {
-
-	u32	val;
-
-	val = ((sz >> 7) + ((sz & 127) ? 1: 0)) << 7;
-
-	return val;
-
+	return ((sz >> 7) + ((sz & 127) ? 1: 0)) << 7;
 }
 
 static inline u32 _RND256(u32 sz)

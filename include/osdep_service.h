166a043c130dbf02d036d80cb8a7d1437a44f46f
Stefan Lippers-Hollmann <s.l-h@gmx.de>
2014-03-22 03:03:01 +0100

    rtl8192du: drop unused function rtw_cbuf_free()
    Signed-off-by: Stefan Lippers-Hollmann <s.l-h@gmx.de>

--- include/osdep_service.h
+++ include/osdep_service.h
@@ -418,6 +418,4 @@ bool rtw_cbuf_empty(struct rtw_cbuf *cbuf);
 bool rtw_cbuf_push(struct rtw_cbuf *cbuf, void *buf);
 void *rtw_cbuf_pop(struct rtw_cbuf *cbuf);
 struct rtw_cbuf *rtw_cbuf_alloc(u32 size);
-void rtw_cbuf_free(struct rtw_cbuf *cbuf);
-
 #endif

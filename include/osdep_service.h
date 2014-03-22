ca98b538e3f76d5c5c1ccefe1ad0119ef3d5f1b3
Stefan Lippers-Hollmann <s.l-h@gmx.de>
2014-03-22 03:03:14 +0100

    rtl8192du: remove unused function rtw_get_time_interval_ms()
    Signed-off-by: Stefan Lippers-Hollmann <s.l-h@gmx.de>

--- include/osdep_service.h
+++ include/osdep_service.h
@@ -200,7 +200,6 @@ u32	rtw_get_current_time(void);
 u32	rtw_systime_to_ms(u32 systime);
 u32	rtw_ms_to_systime(u32 ms);
 s32	rtw_get_passing_time_ms(u32 start);
-s32	rtw_get_time_interval_ms(u32 start, u32 end);
 
 void	rtw_sleep_schedulable(int ms);
 

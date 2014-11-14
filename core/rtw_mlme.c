lwfinger/rtl8192du

commit d88f1fa05583fae4b2a0020b6bcbe71e6064af10

rtl8192du: Fix sparse and W=1 warnings
This commit fixes all the warnings that are issued with W=1 on the make line.

Most sparse warnings are also fixed; however, some of the endian issues remain.

Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>

core/rtw_mlme.c
@@ -560,7 +560,7 @@ int rtw_is_same_ibss(struct rtw_adapter *adapter, struct wlan_network *pnetwork)
 	return ret;
 }
 
-inline int is_same_ess(struct wlan_bssid_ex *a, struct wlan_bssid_ex *b)
+static inline int is_same_ess(struct wlan_bssid_ex *a, struct wlan_bssid_ex *b)
 {
 	/* RT_TRACE(_module_rtl871x_mlme_c_,_drv_err_,("(%s,%d)(%s,%d)\n", */
 	/*              a->Ssid.Ssid,a->Ssid.SsidLength,b->Ssid.Ssid,b->Ssid.SsidLength)); */
@@ -1612,7 +1612,9 @@ static void rtw_joinbss_update_network(struct rtw_adapter *padapter,
 /* define REJOIN */
 void rtw_joinbss_event_prehandle(struct rtw_adapter *adapter, u8 *pbuf)
 {
+#ifdef REJOIN
 	static u8 retry;
+#endif
 	u8 timer_cancelled;
 	struct sta_info *ptarget_sta = NULL, *pcur_sta = NULL;
 	struct sta_priv *pstapriv = &adapter->stapriv;
@@ -1657,7 +1659,9 @@ void rtw_joinbss_event_prehandle(struct rtw_adapter *adapter, u8 *pbuf)
 
 	if (pnetwork->join_res > 0) {
 		spin_lock_bh(&(pmlmepriv->scanned_queue.lock));
+#ifdef REJOIN
 		retry = 0;
+#endif
 		if (check_fwstate(pmlmepriv, _FW_UNDER_LINKING)) {
 			/* s1. find ptarget_wlan */
 			if (check_fwstate(pmlmepriv, _FW_LINKED)) {
@@ -2958,8 +2962,8 @@ unsigned int rtw_restructure_ht_ie(struct rtw_adapter *padapter, u8 *in_ie,
 				   u8 *out_ie, uint in_len, uint *pout_len,
 				   u8 channel)
 {
-	u32 ielen, out_len;
-	unsigned char *p, *pframe;
+	u32 ielen;
+	unsigned char *p;
 	struct rtw_ieee80211_ht_cap ht_capie;
 	unsigned char WMM_IE[] = { 0x00, 0x50, 0xf2, 0x02, 0x00, 0x01, 0x00 };
 	struct mlme_priv *pmlmepriv = &padapter->mlmepriv;
@@ -2973,16 +2977,8 @@ unsigned int rtw_restructure_ht_ie(struct rtw_adapter *padapter, u8 *in_ie,
 	p = rtw_get_ie(in_ie + 12, _HT_CAPABILITY_IE_, &ielen, in_len - 12);
 
 	if (p && ielen > 0) {
-		if (pqospriv->qos_option == 0) {
-			out_len = *pout_len;
-			pframe =
-			    rtw_set_ie(out_ie + out_len, _VENDOR_SPECIFIC_IE_,
-				       _WMM_IE_Length_, WMM_IE, pout_len);
-
+		if (pqospriv->qos_option == 0)
 			pqospriv->qos_option = 1;
-		}
-
-		out_len = *pout_len;
 
 		memset(&ht_capie, 0, sizeof(struct rtw_ieee80211_ht_cap));
 
@@ -3008,10 +3004,6 @@ unsigned int rtw_restructure_ht_ie(struct rtw_adapter *padapter, u8 *in_ie,
 					    &rx_packet_offset);
 			rtw_hal_get_def_var(padapter, HAL_DEF_MAX_RECVBUF_SZ,
 					    &max_recvbuf_sz);
-			/* if (max_recvbuf_sz-rx_packet_offset>(8191-256)) { */
-			/*      DBG_8192D("%s IEEE80211_HT_CAP_MAX_AMSDU is set\n", __func__); */
-			/*      ht_capie.cap_info = ht_capie.cap_info |IEEE80211_HT_CAP_MAX_AMSDU; */
-			/*  */
 		}
 
 		ht_capie.ampdu_params_info =
@@ -3024,19 +3016,10 @@ unsigned int rtw_restructure_ht_ie(struct rtw_adapter *padapter, u8 *in_ie,
 			ht_capie.ampdu_params_info |=
 			    (IEEE80211_HT_CAP_AMPDU_DENSITY & 0x00);
 
-		pframe = rtw_set_ie(out_ie + out_len, _HT_CAPABILITY_IE_,
-				    sizeof(struct rtw_ieee80211_ht_cap),
-				    (unsigned char *)&ht_capie, pout_len);
 		phtpriv->ht_option = true;
 
 		p = rtw_get_ie(in_ie + 12, _HT_ADD_INFO_IE_, &ielen,
 			       in_len - 12);
-		if (p && (ielen == sizeof(struct ieee80211_ht_addt_info))) {
-			out_len = *pout_len;
-			pframe =
-			    rtw_set_ie(out_ie + out_len, _HT_ADD_INFO_IE_,
-				       ielen, p + 2, pout_len);
-		}
 	}
 	return phtpriv->ht_option;
 }
@@ -3049,7 +3032,6 @@ void rtw_update_ht_cap(struct rtw_adapter *padapter, u8 *pie, uint ie_len,
 	int len;
 	/* struct sta_info *bmc_sta, *psta; */
 	struct rtw_ieee80211_ht_cap *pht_capie;
-	struct ieee80211_ht_addt_info *pht_addtinfo;
 	/* struct recv_reorder_ctrl *preorder_ctrl; */
 	struct mlme_priv *pmlmepriv = &padapter->mlmepriv;
 	struct ht_priv *phtpriv = &pmlmepriv->htpriv;
@@ -3100,10 +3082,6 @@ void rtw_update_ht_cap(struct rtw_adapter *padapter, u8 *pie, uint ie_len,
 	p = rtw_get_ie(pie + sizeof(struct ndis_802_11_fixed_ies),
 		       _HT_ADD_INFO_IE_, &len,
 		       ie_len - sizeof(struct ndis_802_11_fixed_ies));
-	if (p && len > 0) {
-		pht_addtinfo = (struct ieee80211_ht_addt_info *)(p + 2);
-		/* todo: */
-	}
 
 	if (channel > 14) {
 		if (pregistrypriv->cbw40_enable & BIT(1))

Makefile
@@ -32,6 +32,7 @@ CONFIG_DRVEXT_MODULE = n
 
 export TopDIR ?= $(shell pwd)
 
+ccflags-y += -D__CHECK_ENDIAN__
 
 RTL871X = rtl8192d
 

core/rtw_br_ext.c
@@ -714,12 +714,12 @@ int nat25_db_handle(struct rtw_adapter *priv, struct sk_buff *skb, int method)
 	if ((method <= NAT25_MIN) || (method >= NAT25_MAX))
 		return -1;
 
-	protocol = *((unsigned short *)(skb->data + 2 * ETH_ALEN));
+	protocol = be16_to_cpu(*((__be16 *)(skb->data + 2 * ETH_ALEN)));
 
 	/*---------------------------------------------------*/
 	/*                 Handle IP frame                   */
 	/*---------------------------------------------------*/
-	if (protocol == __constant_htons(ETH_P_IP)) {
+	if (protocol == ETH_P_IP) {
 		struct iphdr *iph = (struct iphdr *)(skb->data + ETH_HLEN);
 
 		if (((unsigned char *)(iph) + (iph->ihl<<2)) >= (skb->data + ETH_HLEN + skb->len)) {
@@ -778,7 +778,7 @@ int nat25_db_handle(struct rtw_adapter *priv, struct sk_buff *skb, int method)
 	/*---------------------------------------------------*/
 	/*                 Handle ARP frame                  */
 	/*---------------------------------------------------*/
-	} else if (protocol == __constant_htons(ETH_P_ARP)) {
+	} else if (protocol == ETH_P_ARP) {
 		struct arphdr *arp = (struct arphdr *)(skb->data + ETH_HLEN);
 		unsigned char *arp_ptr = (unsigned char *)(arp + 1);
 		unsigned int *sender, *target;
@@ -831,18 +831,18 @@ int nat25_db_handle(struct rtw_adapter *priv, struct sk_buff *skb, int method)
 	/*---------------------------------------------------*/
 	/*         Handle IPX and Apple Talk frame           */
 	/*---------------------------------------------------*/
-	} else if ((protocol == __constant_htons(ETH_P_IPX)) ||
-		   (protocol <= __constant_htons(ETH_FRAME_LEN))) {
+	} else if ((protocol == ETH_P_IPX) ||
+		   (protocol <= ETH_FRAME_LEN)) {
 		unsigned char ipx_header[2] = {0xFF, 0xFF};
 		struct ipxhdr	*ipx = NULL;
 		struct elapaarp	*ea = NULL;
 		struct ddpehdr	*ddp = NULL;
 		unsigned char *frameptr = skb->data + ETH_HLEN;
 
-		if (protocol == __constant_htons(ETH_P_IPX)) {
+		if (protocol == ETH_P_IPX) {
 			DBG_8192D("NAT25: Protocol = IPX (Ethernet II)\n");
 			ipx = (struct ipxhdr *)frameptr;
-		} else if (protocol <= __constant_htons(ETH_FRAME_LEN)) {
+		} else if (protocol <= ETH_FRAME_LEN) {
 			if (!memcmp(ipx_header, frameptr, 2)) {
 				DBG_8192D("NAT25: Protocol = IPX (Ethernet 802.3)\n");
 				ipx = (struct ipxhdr *)frameptr;
@@ -1032,8 +1032,8 @@ int nat25_db_handle(struct rtw_adapter *priv, struct sk_buff *skb, int method)
 	/*---------------------------------------------------*/
 	/*                Handle PPPoE frame                 */
 	/*---------------------------------------------------*/
-	} else if ((protocol == __constant_htons(ETH_P_PPP_DISC)) ||
-		(protocol == __constant_htons(ETH_P_PPP_SES))) {
+	} else if ((protocol == ETH_P_PPP_DISC) ||
+		(protocol == ETH_P_PPP_SES)) {
 		struct pppoe_hdr *ph = (struct pppoe_hdr *)(skb->data + ETH_HLEN);
 		unsigned short *magic;
 
@@ -1042,7 +1042,6 @@ int nat25_db_handle(struct rtw_adapter *priv, struct sk_buff *skb, int method)
 			if (ph->sid == 0)
 				return 0;
 			return 1;
-
 		case NAT25_INSERT:
 			if (ph->sid == 0) {	/*  Discovery phase according to tag */
 				if (ph->code == PADI_CODE || ph->code == PADR_CODE) {
@@ -1059,20 +1058,16 @@ int nat25_db_handle(struct rtw_adapter *priv, struct sk_buff *skb, int method)
 								ERR_8192D("SID tag length too long!\n");
 								return -1;
 							}
-
 							memcpy(tag->tag_data+MAGIC_CODE_LEN+RTL_RELAY_TAG_LEN,
 							       old_tag->tag_data, old_tag_len);
-
 							if (skb_pull_and_merge(skb, (unsigned char *)old_tag, TAG_HDR_LEN+old_tag_len) < 0) {
 								ERR_8192D("call skb_pull_and_merge() failed in PADI/R packet!\n");
 								return -1;
 							}
 							ph->length = htons(ntohs(ph->length)-TAG_HDR_LEN-old_tag_len);
 						}
-
 						tag->tag_type = PTT_RELAY_SID;
 						tag->tag_len = htons(MAGIC_CODE_LEN+RTL_RELAY_TAG_LEN+old_tag_len);
-
 						/*  insert the magic_code+client mac in relay tag */
 						magic = (unsigned short *)tag->tag_data;
 						*magic = htons(MAGIC_CODE);
@@ -1081,7 +1076,6 @@ int nat25_db_handle(struct rtw_adapter *priv, struct sk_buff *skb, int method)
 						/* Add relay tag */
 						if (__nat25_add_pppoe_tag(skb, tag) < 0)
 							return -1;
-
 						DBG_8192D("NAT25: Insert PPPoE, forward %s packet\n",
 							  (ph->code == PADI_CODE ? "PADI" : "PADR"));
 					} else { /*  not add relay tag */
@@ -1090,10 +1084,8 @@ int nat25_db_handle(struct rtw_adapter *priv, struct sk_buff *skb, int method)
 							ERR_8192D("Discard PPPoE packet due to another PPPoE connection is in progress!\n");
 							return -2;
 						}
-
 						if (priv->pppoe_connection_in_progress == 0)
 							memcpy(priv->pppoe_addr, skb->data+ETH_ALEN, ETH_ALEN);
-
 						priv->pppoe_connection_in_progress = WAIT_TIME_PPPOE;
 					}
 				} else {
@@ -1101,13 +1093,9 @@ int nat25_db_handle(struct rtw_adapter *priv, struct sk_buff *skb, int method)
 				}
 			} else {	/*  session phase */
 				DBG_8192D("NAT25: Insert PPPoE, insert session packet to %s\n", skb->dev->name);
-
 				__nat25_generate_pppoe_network_addr(networkAddr, skb->data, &(ph->sid));
-
 				__nat25_db_network_insert(priv, skb->data+ETH_ALEN, networkAddr);
-
 				__nat25_db_print(priv);
-
 				if (!priv->eth_br_ext_info.addPPPoETag &&
 				    priv->pppoe_connection_in_progress &&
 				    !memcmp(skb->data+ETH_ALEN, priv->pppoe_addr, ETH_ALEN))
@@ -1127,11 +1115,9 @@ int nat25_db_handle(struct rtw_adapter *priv, struct sk_buff *skb, int method)
 						ERR_8192D("Fail to find PTT_RELAY_SID in FADO!\n");
 						return -1;
 					}
-
 					tag = (struct pppoe_tag *)ptr;
 					tagType = (unsigned short)((ptr[0] << 8) + ptr[1]);
 					tagLen = (unsigned short)((ptr[2] << 8) + ptr[3]);
-
 					if ((tagType != ntohs(PTT_RELAY_SID)) || (tagLen < (MAGIC_CODE_LEN+RTL_RELAY_TAG_LEN))) {
 						ERR_8192D("Invalid PTT_RELAY_SID tag length [%d]!\n", tagLen);
 						return -1;
@@ -1171,9 +1157,7 @@ int nat25_db_handle(struct rtw_adapter *priv, struct sk_buff *skb, int method)
 				if (ph->sid != 0) {
 					DBG_8192D("NAT25: Lookup PPPoE, lookup session packet from %s\n", skb->dev->name);
 					__nat25_generate_pppoe_network_addr(networkAddr, skb->data+ETH_ALEN, &(ph->sid));
-
 					__nat25_db_network_lookup_and_replace(priv, skb, networkAddr);
-
 					__nat25_db_print(priv);
 				} else {
 					return -1;
@@ -1187,17 +1171,14 @@ int nat25_db_handle(struct rtw_adapter *priv, struct sk_buff *skb, int method)
 	/*---------------------------------------------------*/
 	/*                 Handle EAP frame                  */
 	/*---------------------------------------------------*/
-	} else if (protocol == __constant_htons(0x888e)) {
+	} else if (protocol == 0x888e) {
 		switch (method) {
 		case NAT25_CHECK:
 			return -1;
-
 		case NAT25_INSERT:
 			return 0;
-
 		case NAT25_LOOKUP:
 			return 0;
-
 		default:
 			return -1;
 		}
@@ -1205,18 +1186,15 @@ int nat25_db_handle(struct rtw_adapter *priv, struct sk_buff *skb, int method)
 	/*---------------------------------------------------*/
 	/*         Handle C-Media proprietary frame          */
 	/*---------------------------------------------------*/
-	} else if ((protocol == __constant_htons(0xe2ae)) ||
-		(protocol == __constant_htons(0xe2af))) {
+	} else if ((protocol == 0xe2ae) ||
+		(protocol == 0xe2af)) {
 		switch (method) {
 		case NAT25_CHECK:
 			return -1;
-
 		case NAT25_INSERT:
 			return 0;
-
 		case NAT25_LOOKUP:
 			return 0;
-
 		default:
 			return -1;
 		}
@@ -1226,7 +1204,7 @@ int nat25_db_handle(struct rtw_adapter *priv, struct sk_buff *skb, int method)
 	/*         Handle IPV6 frame								  */
 	/*---------------------------------------------------*/
 #ifdef CL_IPV6_PASS
-	else if (protocol == __constant_htons(ETH_P_IPV6)) {
+	else if (protocol == ETH_P_IPV6) {
 		struct ipv6hdr *iph = (struct ipv6hdr *)(skb->data + ETH_HLEN);
 
 		if (sizeof(*iph) >= (skb->len - ETH_HLEN)) {
@@ -1240,23 +1218,9 @@ int nat25_db_handle(struct rtw_adapter *priv, struct sk_buff *skb, int method)
 				return 0;
 			return -1;
 		case NAT25_INSERT:
-			DBG_8192D("NAT25: Insert IP, SA =%4x:%4x:%4x:%4x:%4x:%4x:%4x:%4x, DA =%4x:%4x:%4x:%4x:%4x:%4x:%4x:%4x\n",
-				  iph->saddr.s6_addr16[0],
-				  iph->saddr.s6_addr16[1],
-				  iph->saddr.s6_addr16[2],
-				  iph->saddr.s6_addr16[3],
-				  iph->saddr.s6_addr16[4],
-				  iph->saddr.s6_addr16[5],
-				  iph->saddr.s6_addr16[6],
-				  iph->saddr.s6_addr16[7],
-				  iph->daddr.s6_addr16[0],
-				  iph->daddr.s6_addr16[1],
-				  iph->daddr.s6_addr16[2],
-				  iph->daddr.s6_addr16[3],
-				  iph->daddr.s6_addr16[4],
-				  iph->daddr.s6_addr16[5],
-				  iph->daddr.s6_addr16[6],
-				  iph->daddr.s6_addr16[7]);
+			DBG_8192D("NAT25: Insert IP, SA =%8ph, DA = %8ph\n",
+				  iph->saddr.s6_addr16,
+				  iph->daddr.s6_addr16);
 
 			if (memcmp(&iph->saddr, "\x0\x0\x0\x0\x0\x0\x0\x0\x0\x0\x0\x0\x0\x0\x0\x0", 16)) {
 				__nat25_generate_ipv6_network_addr(networkAddr, (unsigned int *)&iph->saddr);
@@ -1271,7 +1235,7 @@ int nat25_db_handle(struct rtw_adapter *priv, struct sk_buff *skb, int method)
 						struct icmp6hdr  *hdr = (struct icmp6hdr *)(skb->data + ETH_HLEN + sizeof(*iph));
 						hdr->icmp6_cksum = 0;
 						hdr->icmp6_cksum = csum_ipv6_magic(&iph->saddr, &iph->daddr,
-										iph->payload_len,
+										be16_to_cpu(iph->payload_len),
 										IPPROTO_ICMPV6,
 										csum_partial((__u8 *)hdr, iph->payload_len, 0));
 					}
@@ -1321,8 +1285,10 @@ int nat25_handle_frame(struct rtw_adapter *priv, struct sk_buff *skb)
 	if (!(skb->data[0] & 1)) {
 		int is_vlan_tag = 0, i, retval = 0;
 		unsigned short vlan_hdr = 0;
+		unsigned short protocol;
 
-		if (*((unsigned short *)(skb->data+ETH_ALEN*2)) == __constant_htons(ETH_P_8021Q)) {
+		protocol = be16_to_cpu(*((__be16 *)(skb->data + 2 * ETH_ALEN)));
+		if (protocol == ETH_P_8021Q) {
 			is_vlan_tag = 1;
 			vlan_hdr = *((unsigned short *)(skb->data+ETH_ALEN*2+2));
 			for (i = 0; i < 6; i++)
@@ -1338,7 +1304,7 @@ int nat25_handle_frame(struct rtw_adapter *priv, struct sk_buff *skb)
 			 *	corresponding network protocol is NOT support.
 			 */
 			if (!priv->eth_br_ext_info.nat25sc_disable &&
-			    (*((unsigned short *)(skb->data+ETH_ALEN*2)) == __constant_htons(ETH_P_IP)) &&
+			    (be16_to_cpu(*((__be16 *)(skb->data+ETH_ALEN*2))) == ETH_P_IP) &&
 			    !memcmp(priv->scdb_ip, skb->data+ETH_HLEN+16, 4)) {
 				memcpy(skb->data, priv->scdb_mac, ETH_ALEN);
 
@@ -1349,9 +1315,9 @@ int nat25_handle_frame(struct rtw_adapter *priv, struct sk_buff *skb)
 				retval = nat25_db_handle(priv, skb, NAT25_LOOKUP);
 			}
 		} else {
-			if (((*((unsigned short *)(skb->data+ETH_ALEN*2)) == __constant_htons(ETH_P_IP)) &&
+			if (((be16_to_cpu(*((__be16 *)(skb->data+ETH_ALEN*2))) == ETH_P_IP) &&
 			     !memcmp(priv->br_ip, skb->data+ETH_HLEN+16, 4)) ||
-			     ((*((unsigned short *)(skb->data+ETH_ALEN*2)) == __constant_htons(ETH_P_ARP)) &&
+			     ((be16_to_cpu(*((__be16 *)(skb->data+ETH_ALEN*2))) == ETH_P_ARP) &&
 			     !memcmp(priv->br_ip, skb->data+ETH_HLEN+24, 4))) {
 				/*  for traffic to upper TCP/IP */
 				retval = nat25_db_handle(priv, skb, NAT25_LOOKUP);
@@ -1362,8 +1328,8 @@ int nat25_handle_frame(struct rtw_adapter *priv, struct sk_buff *skb)
 			skb_push(skb, 4);
 			for (i = 0; i < 6; i++)
 				*((unsigned short *)(skb->data+i*2)) = *((unsigned short *)(skb->data+4+i*2));
-			*((unsigned short *)(skb->data+ETH_ALEN*2)) = __constant_htons(ETH_P_8021Q);
-			*((unsigned short *)(skb->data+ETH_ALEN*2+2)) = vlan_hdr;
+			*((__be16 *)(skb->data+ETH_ALEN*2)) = __constant_htons(ETH_P_8021Q);
+			*((__be16 *)(skb->data+ETH_ALEN*2+2)) = cpu_to_be16(vlan_hdr);
 		}
 
 		if (retval == -1)
@@ -1392,7 +1358,7 @@ struct dhcp_message {
 	u_int8_t chaddr[16];
 	u_int8_t sname[64];
 	u_int8_t file[128];
-	u_int32_t cookie;
+	__be32 cookie;
 	u_int8_t options[308]; /* 312 - cookie */
 };
 
@@ -1402,7 +1368,7 @@ void dhcp_flag_bcast(struct rtw_adapter *priv, struct sk_buff *skb)
 		return;
 
 	if (!priv->eth_br_ext_info.dhcp_bcst_disable) {
-		unsigned short protocol = *((unsigned short *)(skb->data + 2 * ETH_ALEN));
+		__be16 protocol = *((__be16 *)(skb->data + 2 * ETH_ALEN));
 
 		if (protocol == __constant_htons(ETH_P_IP)) { /*  IP */
 			struct iphdr *iph = (struct iphdr *)(skb->data + ETH_HLEN);
@@ -1414,8 +1380,9 @@ void dhcp_flag_bcast(struct rtw_adapter *priv, struct sk_buff *skb)
 				    (udph->dest == __constant_htons(SERVER_PORT))) { /*  DHCP request */
 					struct dhcp_message *dhcph =
 						(struct dhcp_message *)((SIZE_PTR)udph + sizeof(struct udphdr));
+					u32 cookie = be32_to_cpu((__be32)dhcph->cookie);
 
-					if (dhcph->cookie == __constant_htonl(DHCP_MAGIC)) { /*  match magic word */
+					if (cookie == DHCP_MAGIC) { /*  match magic word */
 						if (!(dhcph->flags & htons(BROADCAST_FLAG))) { /*  if not broadcast */
 							register int sum = 0;
 

core/rtw_cmd.c
@@ -367,7 +367,7 @@ int rtw_cmd_thread(void *context)
 {
 	u8 ret;
 	struct cmd_obj *pcmd;
-	u8 *pcmdbuf, *prspbuf;
+	u8 *pcmdbuf;
 	u8 (*cmd_hdl)(struct rtw_adapter *padapter, u8 *pbuf);
 	void (*pcmd_callback)(struct rtw_adapter *dev, struct cmd_obj *pcmd);
 	struct rtw_adapter *padapter = (struct rtw_adapter *)context;
@@ -378,7 +378,6 @@ _func_enter_;
 	thread_enter("RTW_CMD_THREAD");
 
 	pcmdbuf = pcmdpriv->cmd_buf;
-	prspbuf = pcmdpriv->rsp_buf;
 
 	pcmdpriv->cmdthd_running = true;
 	_rtw_up_sema(&pcmdpriv->terminate_cmdthread_sema);
@@ -1022,7 +1021,7 @@ _func_exit_;
 
 u8 rtw_joinbss_cmd(struct rtw_adapter *padapter, struct wlan_network *pnetwork)
 {
-	u8 *auth, res = _SUCCESS;
+	u8 res = _SUCCESS;
 	uint	t_len = 0;
 	struct wlan_bssid_ex	*psecnetwork;
 	struct cmd_obj *pcmd;
@@ -1084,7 +1083,6 @@ _func_enter_;
 
 	memcpy(psecnetwork, &pnetwork->network, get_wlan_bssid_ex_sz(&pnetwork->network));
 
-	auth = &psecuritypriv->authenticator_ie[0];
 	psecuritypriv->authenticator_ie[0] = (unsigned char)psecnetwork->IELength;
 
 	if ((psecnetwork->IELength-12) < (256-1)) {

core/rtw_debug.c
@@ -98,7 +98,7 @@ int proc_set_log_level(struct file *file, const char __user *buffer,
 	struct net_device *dev = (struct net_device *)data;
 	struct rtw_adapter *padapter = (struct rtw_adapter *)rtw_netdev_priv(dev);
 	char tmp[32];
-	u32 is_signal_dbg;
+	s32 is_signal_dbg;
 
 	if (count < 1)
 		return -EFAULT;
@@ -673,7 +673,8 @@ int proc_set_rx_signal(struct file *file, const char __user *buffer,
 	struct net_device *dev = (struct net_device *)data;
 	struct rtw_adapter *padapter = (struct rtw_adapter *)rtw_netdev_priv(dev);
 	char tmp[32];
-	u32 is_signal_dbg, signal_strength;
+	u32 is_signal_dbg;
+	s32 signal_strength;
 
 	if (count < 1)
 		return -EFAULT;
@@ -727,7 +728,7 @@ int proc_set_ht_enable(struct file *file, const char __user *buffer,
 	struct rtw_adapter *padapter = (struct rtw_adapter *)rtw_netdev_priv(dev);
 	struct registry_priv	*pregpriv = &padapter->registrypriv;
 	char tmp[32];
-	u32 mode;
+	s32 mode;
 
 	if (count < 1)
 		return -EFAULT;
@@ -771,7 +772,7 @@ int proc_set_cbw40_enable(struct file *file, const char __user *buffer,
 	struct rtw_adapter *padapter = (struct rtw_adapter *)rtw_netdev_priv(dev);
 	struct registry_priv	*pregpriv = &padapter->registrypriv;
 	char tmp[32];
-	u32 mode;
+	s32 mode;
 
 	if (count < 1)
 		return -EFAULT;
@@ -814,7 +815,7 @@ int proc_set_ampdu_enable(struct file *file, const char __user *buffer,
 	struct rtw_adapter *padapter = (struct rtw_adapter *)rtw_netdev_priv(dev);
 	struct registry_priv	*pregpriv = &padapter->registrypriv;
 	char tmp[32];
-	u32 mode;
+	s32 mode;
 
 	if (count < 1)
 		return -EFAULT;

core/rtw_efuse.c
@@ -479,7 +479,7 @@ u8 rtw_efuse_map_write(struct rtw_adapter *padapter, u16 addr, u16 cnts, u8 *dat
 	u8 offset, word_en;
 	u8 *map;
 	u8 newdata[PGPKT_DATA_SIZE];
-	s32	i, j, idx;
+	s32	i, idx;
 	u8 ret = _SUCCESS;
 	u16 maplen = 0;
 
@@ -502,7 +502,6 @@ u8 rtw_efuse_map_write(struct rtw_adapter *padapter, u16 addr, u16 cnts, u8 *dat
 	word_en = 0xF;
 	memset(newdata, 0xFF, PGPKT_DATA_SIZE);
 	i = addr & 0x7;	/*  index of one package */
-	j = 0;		/*  index of new package */
 	idx = 0;	/*  data index */
 
 	if (i & 0x1) {
@@ -556,7 +555,6 @@ u8 rtw_efuse_map_write(struct rtw_adapter *padapter, u16 addr, u16 cnts, u8 *dat
 
 		offset++;
 		i = 0;
-		j = 0;
 		word_en = 0xF;
 		memset(newdata, 0xFF, PGPKT_DATA_SIZE);
 	} while (1);

core/rtw_ieee80211.c
@@ -651,7 +651,7 @@ int rtw_parse_wpa2_ie(u8 *rsn_ie, int rsn_ie_len, int *group_cipher, int *pairwi
 
 int rtw_get_sec_ie(u8 *in_ie, uint in_len, u8 *rsn_ie, u16 *rsn_len, u8 *wpa_ie, u16 *wpa_len)
 {
-	u8 authmode, sec_idx, i;
+	u8 authmode, i;
 	u8 wpa_oui[4] = {0x0, 0x50, 0xf2, 0x01};
 	uint	cnt;
 
@@ -661,16 +661,14 @@ _func_enter_;
 
 	cnt = (_TIMESTAMP_ + _BEACON_ITERVAL_ + _CAPABILITY_);
 
-	sec_idx = 0;
-
 	while (cnt < in_len) {
 		authmode = in_ie[cnt];
 
 		if ((authmode == _WPA_IE_ID_) &&
 		    (_rtw_memcmp(&in_ie[cnt+2], &wpa_oui[0], 4) == true)) {
 				RT_TRACE(_module_rtl871x_mlme_c_, _drv_info_,
-					 ("\n rtw_get_wpa_ie: sec_idx =%d in_ie[cnt+1]+2 =%d\n",
-					 sec_idx, in_ie[cnt+1]+2));
+					 ("\n rtw_get_wpa_ie: in_ie[cnt+1]+2 =%d\n",
+					 in_ie[cnt+1]+2));
 
 				memcpy(wpa_ie, &in_ie[cnt], in_ie[cnt+1]+2);
 

core/rtw_ioctl_set.c
@@ -551,16 +551,12 @@ _func_exit_;
 
 u8 rtw_set_802_11_add_wep(struct rtw_adapter *padapter, struct ndis_802_11_wep *wep)
 {
-	u8		bdefaultkey;
-	u8		btransmitkey;
 	int		keyid, res;
 	struct security_priv *psecuritypriv = &(padapter->securitypriv);
 	u8		ret = _SUCCESS;
 
 _func_enter_;
 
-	bdefaultkey = (wep->KeyIndex & 0x40000000) > 0 ? false : true;   /* for ??? */
-	btransmitkey = (wep->KeyIndex & 0x80000000) > 0 ? true  : false;	/* for ??? */
 	keyid = wep->KeyIndex & 0x3fffffff;
 
 	if (keyid > 4) {
@@ -837,7 +833,6 @@ _func_enter_;
 	/*  If WEP encryption algorithm, just call rtw_set_802_11_add_wep(). */
 	if ((padapter->securitypriv.dot11AuthAlgrthm != dot11AuthAlgrthm_8021X) && (encryptionalgo == _WEP40_ ||
 	    encryptionalgo == _WEP104_)) {
-		u8 ret;
 		u32 keyindex;
 		u32 len = FIELD_OFFSET(struct ndis_802_11_key, KeyMaterial) + key->KeyLength;
 		struct ndis_802_11_wep *wep = &padapter->securitypriv.ndiswep;
@@ -997,7 +992,6 @@ _func_exit_;
 u8 rtw_set_802_11_remove_key(struct rtw_adapter *padapter,
 			     struct ndis_802_11_remove_key *key)
 {
-	uint	encryptionalgo;
 	u8 *pbssid;
 	struct sta_info *stainfo;
 	u8	bgroup = (key->KeyIndex & 0x4000000) > 0 ? false : true;
@@ -1011,7 +1005,6 @@ _func_enter_;
 		goto exit;
 	}
 	if (bgroup == true) {
-		encryptionalgo = padapter->securitypriv.dot118021XGrpPrivacy;
 		/*  clear group key by index */
 
 		memset(&padapter->securitypriv.dot118021XGrpKey[keyIndex], 0, 16);
@@ -1021,8 +1014,6 @@ _func_enter_;
 		pbssid = get_bssid(&padapter->mlmepriv);
 		stainfo = rtw_get_stainfo(&padapter->stapriv, pbssid);
 		if (stainfo != NULL) {
-			encryptionalgo = stainfo->dot118021XPrivacy;
-
 			/*  clear key by BSSID */
 			memset(&stainfo->dot118021x_UncstKey, 0, 16);
 			/*  \todo Send a H2C Command to Firmware for disable this Key in CAM Entry. */
@@ -1036,7 +1027,7 @@ exit:
 
 _func_exit_;
 
-	return true;
+	return ret;
 }
 
 /*

commit 3903bb3ca62db2efcc42bf6efb00925f174d2ebc

rtl8192du: Partially restore 5G performance
The original driver is capable of about 80 Mbps when running netperf
TCP tests. Commit d88f1fa introduced an error that cut the throughput
to about 30 Mbps. These changes improve the results to RX of 46 and TX
of 60 Mbps.

Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>

core/rtw_mlme.c
@@ -498,8 +498,6 @@ int rtw_is_same_ibss(struct rtw_adapter *adapter, struct wlan_network *pnetwork)
 
 static inline int is_same_ess(struct wlan_bssid_ex *a, struct wlan_bssid_ex *b)
 {
-	/* RT_TRACE(_module_rtl871x_mlme_c_,_drv_err_,("(%s,%d)(%s,%d)\n", */
-	/*              a->Ssid.Ssid,a->Ssid.SsidLength,b->Ssid.Ssid,b->Ssid.SsidLength)); */
 	return (a->Ssid.SsidLength == b->Ssid.SsidLength) &&
 		_rtw_memcmp(a->Ssid.Ssid, b->Ssid.Ssid,
 			    a->Ssid.SsidLength) == true;
@@ -517,7 +515,6 @@ int is_same_network(struct wlan_bssid_ex *src, struct wlan_bssid_ex *dst)
 	d_cap = le16_to_cpu(le_dcap);
 
 	return ((src->Ssid.SsidLength == dst->Ssid.SsidLength) &&
-		/*      (src->Configuration.DSConfig == dst->Configuration.DSConfig) && */
 		((_rtw_memcmp(src->MacAddress, dst->MacAddress, ETH_ALEN)) == true) &&
 		((_rtw_memcmp
 		  (src->Ssid.Ssid, dst->Ssid.Ssid,
@@ -2667,10 +2664,10 @@ unsigned int rtw_restructure_ht_ie(struct rtw_adapter *padapter, u8 *in_ie,
 				   u8 *out_ie, uint in_len, uint *pout_len,
 				   u8 channel)
 {
-	u32 ielen;
-	unsigned char *p;
+	u32 ielen, out_len;
+	unsigned char *p, *pframe;
 	struct rtw_ieee80211_ht_cap ht_capie;
-	unsigned char WMM_IE[] = { 0x00, 0x50, 0xf2, 0x02, 0x00, 0x01, 0x00 };
+	unsigned char WMM_IE[] = {0x00, 0x50, 0xf2, 0x02, 0x00, 0x01, 0x00};
 	struct mlme_priv *pmlmepriv = &padapter->mlmepriv;
 	struct qos_priv *pqospriv = &pmlmepriv->qospriv;
 	struct ht_priv *phtpriv = &pmlmepriv->htpriv;
@@ -2682,8 +2679,16 @@ unsigned int rtw_restructure_ht_ie(struct rtw_adapter *padapter, u8 *in_ie,
 	p = rtw_get_ie(in_ie + 12, _HT_CAPABILITY_IE_, &ielen, in_len - 12);
 
 	if (p && ielen > 0) {
-		if (pqospriv->qos_option == 0)
+		if (pqospriv->qos_option == 0) {
+			out_len = *pout_len;
+			pframe =
+			    rtw_set_ie(out_ie + out_len, _VENDOR_SPECIFIC_IE_,
+				       _WMM_IE_Length_, WMM_IE, pout_len);
+
 			pqospriv->qos_option = 1;
+		}
+
+		out_len = *pout_len;
 
 		memset(&ht_capie, 0, sizeof(struct rtw_ieee80211_ht_cap));
 
@@ -2721,10 +2726,19 @@ unsigned int rtw_restructure_ht_ie(struct rtw_adapter *padapter, u8 *in_ie,
 			ht_capie.ampdu_params_info |=
 			    (IEEE80211_HT_CAP_AMPDU_DENSITY & 0x00);
 
+		pframe = rtw_set_ie(out_ie + out_len, _HT_CAPABILITY_IE_,
+				    sizeof(struct rtw_ieee80211_ht_cap),
+				    (unsigned char *)&ht_capie, pout_len);
 		phtpriv->ht_option = true;
 
 		p = rtw_get_ie(in_ie + 12, _HT_ADD_INFO_IE_, &ielen,
 			       in_len - 12);
+		if (p && (ielen == sizeof(struct ieee80211_ht_addt_info))) {
+			out_len = *pout_len;
+			pframe =
+			    rtw_set_ie(out_ie + out_len, _HT_ADD_INFO_IE_,
+				       ielen, p + 2, pout_len);
+		}
 	}
 	return phtpriv->ht_option;
 }
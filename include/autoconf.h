19cc75cc3bf54d8dee0d42451b962a93da46291f
Larry Finger <Larry.Finger@lwfinger.net>
2018-03-12 00:46:02 -0500

    rtl8192du: Turn on CONFIG_CONCURRENT_MODE
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>

--- include/autoconf.h
+++ include/autoconf.h
@@ -162,7 +162,7 @@
 	#endif
 #endif
 
-//#define CONFIG_CONCURRENT_MODE 1
+#define CONFIG_CONCURRENT_MODE 1
 #ifdef CONFIG_CONCURRENT_MODE
 	#define CONFIG_TSF_RESET_OFFLOAD 1			// For 2 PORT TSF SYNC.
 	//#define CONFIG_HWPORT_SWAP				//Port0->Sec , Port1 -> Pri

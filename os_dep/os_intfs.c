c01872d0a6ede781d13d7ece0a21bfba6ba07c56
Larry Finger <Larry.Finger@lwfinger.net>
2018-11-09 15:16:18 -0600

    rtl8192du: Fix typo for builds with kernel 4.18-
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>

--- os_dep/os_intfs.c
+++ os_dep/os_intfs.c
@@ -467,10 +467,6 @@ static u16 rtw_select_queue(struct net_device *dev, struct sk_buff *skb
 #elif (LINUX_VERSION_CODE >= KERNEL_VERSION(3, 14, 0))
  			    ,void *unused
                              ,select_queue_fallback_t fallback
-					, void *accel_priv
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(3, 14, 0)
-				, select_queue_fallback_t fallback
-#endif
 #endif
 )
 {
